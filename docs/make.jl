7d67f70f9f0fb0974f3a7e3737a1eb467503a16d
Matthew C. Kelley <matthew.curtis.kelley@gmail.com>
2025-11-06 20:15:08 -0500

    Add plotting_tips.md to make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, Phonetics
 ENV["GKSwstype"] = "100"
 
 makedocs(sitename = "Phonetics.jl",
-         pages = ["index.md", "vowelplot.md", "acd.md", "lc.md", "textvptree.md", "phon_spectrogram.md"],
+         pages = ["index.md", "vowelplot.md", "acd.md", "lc.md", "textvptree.md", "phon_spectrogram.md", "plotting_tips.md"],
          repo = "https://github.com/maetshju/Phonetics.jl")
 
 deploydocs(
